c10ef46040f149635f38632e0b6054d0f380bebf
benshabbat <benshabbat27@gmail.com>
2023-09-01 09:31:31 +0300

    start register home

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo"Check working good?" ?>
+<?php echo"Check working good?<br> "?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <label>Home</label><br>
+    <input type="text" name="home"/><br>
+    <label>Address</label><br>
+    <input type="text" name="address"/><br>
+    <label>Rooms</label><br>
+    <input type="number" name="rooms" min=1/><br>
+    <label>Cost</label><br>
+    <input type="number" name="cost" min=0/><br>
+</body>
+</html>

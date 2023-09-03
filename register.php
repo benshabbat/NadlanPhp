7773e981a0df72112e9c829621d2a6cd577ba948
benshabbat <benshabbat27@gmail.com>
2023-09-03 22:44:50 +0300

    create register

---
+++ register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Nadlan</title>
+</head>
+
+<body>
+    <nav class="nav-bar">
+        <a>Logo</a>
+        <a>Login</a>
+    </nav>
+    <form action="index.php" method="post">
+        <h2>Register</h2>
+        <label>Username</label><br>
+        <input type="text" name="username" /><br>
+        <label>Email</label><br>
+        <input type="email" name="email" /><br>
+        <label>Phone</label><br>
+        <input type="phone" name="phone" /><br>
+        <label>Password</label><br>
+        <input type="password" name="password" /><br>
+        <label>Password</label><br>
+        <input type="password" name="password2" /><br>
+        <button type="submit">Register</button>
+    </form>
+
+</body>
+
+</html>

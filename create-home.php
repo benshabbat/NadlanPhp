fff54015803e06598ba5a4dc5da1a2c62276c11c
benshabbat <benshabbat27@gmail.com>
2023-09-03 22:59:38 +0300

    add create-home

---
+++ create-home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Nadlan</title>
+</head>
+
+<body>
+    <nav class="nav-bar">
+        <a>Logo</a>
+        <a>Login</a>
+    </nav>
+    <form action="index.php" method="post">
+        <h2>Create Home</h2>
+        <label>Property Type</label><br>
+        <input type="text" name="propertyType" /><br>
+        <label>City</label><br>
+        <input type="text" name="city" /><br>
+        <label>Address</label><br>
+        <input type="text" name="address" /><br>
+        <label>Photos</label><br>
+        <input type="file" name="photos" /><br>
+        <label>Desription</label><br>
+        <input type="text" name="description" /><br>
+        <label>Perks</label><br>
+        <input type="text" name="perks" /><br>
+        <label>Price</label><br>
+        <input type="slide" name="price" /><br>
+        <label>Rooms</label><br>
+        <input type="number" name="rooms" /><br>
+        <button type="submit">Create</button>
+    </form>
+
+</body>
+
+</html>

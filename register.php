e13900c24523dc0c2dc60079f65a3b890ff39115
benshabbat <benshabbat27@gmail.com>
2023-09-05 00:18:14 +0300

    create sql for user register

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
-include "./html/header.html"
-
+include "./html/header.html";
+include "./database.php";
 ?>
 
 <!DOCTYPE html>
@@ -35,12 +35,27 @@ include "./html/header.html"
 </html>
 <?php
 if (isset($_POST['register'])) {
-
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
     $password = $_POST['password'];
-    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $password2 = $_POST['password2'];
+    if (strcmp($password, $password2) == 0) {
+
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+    }
+    $sql = "INSERT INTO users (username, email, phone, password) VALUES ('$username', '$email','$phone', ' $hash')";
+
+    try {
+        mysqli_query($conn, $sql);
+        echo "user is now registered";
+    } catch (mysqli_sql_exception) {
+        echo "you are cant register";
+    }
 }
-echo password_verify($password,  $hash).'<br>';
+echo password_verify($password,  $hash) . '<br>';
 
 echo $hash;
+mysqli_close($conn)
 
 ?>

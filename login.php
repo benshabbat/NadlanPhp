a30d71019c63456b9e7a4eca2c4bcb9280b2084c
benshabbat <benshabbat27@gmail.com>
2023-09-05 23:31:14 +0300

    some css

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-include "./html/header.html"
-
+include "./html/header.html";
+include "./database.php";
 ?>
 
 <!DOCTYPE html>
@@ -9,12 +9,12 @@ include "./html/header.html"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
     <title>Nadlan</title>
 </head>
 
 <body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
         <h2>Login</h2>
         <label>Username</label><br>
         <input type="text" name="username" /><br>
@@ -28,8 +28,8 @@ include "./html/header.html"
 </html>
 <?php
 
-if(isset($_POST['login'])){
-    $username = filter_input(INPUT_POST,"username",FILTER_SANITIZE_SPECIAL_CHARS)
-
+if (isset($_POST['login'])) {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 }
 ?>

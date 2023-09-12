4287e610c56db1feb9f850309eb7bdd6205694c5
benshabbat <benshabbat27@gmail.com>
2023-09-12 13:32:28 +0300

    add menu for navbar to responsible

--- inc/header.php
+++ inc/header.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Nadlan</title>
-  </head>
-  <body>
-    <header class="header">
-      <a class="logo" href="index.php">Logo</a>
-      <nav class="navbar">
-        <a href="login.php">Login</a>
-      </nav>
-    </header>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <title>Nadlan</title>
+</head>
+
+<body>
+  <header class="header">
+    <a class="logo" href="index.php">Logo</a>
+    <label for="" class="icons">
+      <i class='bx bx-menu'></i>
+    </label>
+    <nav class="navbar">
+      <a href="login.php">Login</a>
+    </nav>
+  </header>
+</body>
+
 </html>

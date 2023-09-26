75b4fe3e59ab6731dd0e105c4d7c3eac23bff7c7
benshabbat <benshabbat27@gmail.com>
2023-09-26 14:18:52 +0300

    clean code

--- houses.php
+++ houses.php
@@ -1,15 +1,8 @@
 <?php
-// include "./config/app.php";
-// include_once "./controllers/AuthController.php";
-// include_once "./controllers/HouseController.php";
-// $authenticated = new AuthController;
-
 include "./inc/header.php";
 ?>
 
 <div>
-
-    <h1>Houses</h1>
     <?php include "./house-view.php"; ?>
 
 </div>
--- properties.php
+++ properties.php
@@ -1,7 +1,5 @@
 <?php
-
 include_once "./controllers/HouseController.php";
-
 include "./inc/header.php";
 ?>
 

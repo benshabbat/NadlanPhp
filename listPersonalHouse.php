8afa21a781bf9f3ad38c9bf46ec062ecd7ec87d8
benshabbat <benshabbat27@gmail.com>
2023-09-29 03:18:44 +0300

    starting new ui for list my houses

---
+++ listPersonalHouse.php
@@ -0,0 +1,15 @@
+<div class="list-container">
+    <div class="left-col">
+        <p>Your Houses</p>
+        <h1></h1>
+        <div class="house">
+            <div class="house-img">
+                <img/>
+            </div>
+            <div class="house-info">
+                <p>desc</p>
+                <p>perks</p>
+            </div>
+        </div>
+    </div>
+</div>
